ddc0bf1740c49628f00091b241afd3b38c04648f
oliullah05 <mdolihasan@gmail.com>
2023-11-30 00:14:14 +0600

    semester logical validation for name== code done in service

--- src/app/modules/academicSemister/academicSemister.service.ts
+++ src/app/modules/academicSemister/academicSemister.service.ts
@@ -2,6 +2,31 @@ import { TAcademicSemister } from "./academicSemister.interface";
 import { AcademicSemester } from "./academicSemister.model";
 
 const createAcademicSemisterIntoDB = async (payload: TAcademicSemister) => {
+
+
+
+    //semister name   ==== semister code 
+    type TAcademicSemisterCodeNameMapper = {
+        [key: string]: string
+    }
+    const academicSemisterCodeNameMapper: TAcademicSemisterCodeNameMapper = {
+        autumn: "01",
+        summer: "02",
+        fall: "03"
+
+    }
+
+    if (academicSemisterCodeNameMapper[payload.name] !== payload.code) {
+        throw new Error("Invaid semister code")
+    }
+
+
+
+
+
+
+
+
     const result = await AcademicSemester.create(payload)
     return result;
 }
