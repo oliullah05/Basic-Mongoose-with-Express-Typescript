3e193f6883ab1774ec0879c5c66494692a58c9ba
oliullah05 <mdolihasan@gmail.com>
2023-12-17 16:19:50 +0600

    check duplicate semesterRegistration  and check if the AcademicSemester exits

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+import { SemesterRegistrationService } from './semesterRegistration.service';
 
 const createSemesterRegistration = catchAsync(
   async (req: Request, res: Response) => {
     
-    // const result =
+    const result = await SemesterRegistrationService.createSemesterRegistrationIntoDB(req.body)
     
-  //   sendResponse(res, {
-  //     statusCode: httpStatus.OK,
-  //     success: true,
-  //     message: 'Semester Registration is created successfully!',
-  //     data: result,
-  //   });
-  // },
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semester Registration is created successfully!',
+      data: result,
+    });
+  },
 );
 
 const getAllSemesterRegistrations = catchAsync(
--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,4 +1,33 @@
-const createSemesterRegistrationIntoDB = async () => {};
+import AppError from "../../errors/AppError";
+import { AcademicSemester } from "../academicSemester/academicSemester.model";
+import { TSemesterRegistration } from "./semesterRegistration.interface";
+import { SemesterRegistration } from "./semesterRegistration.model";
+
+const createSemesterRegistrationIntoDB = async (payload:TSemesterRegistration) => {
+
+  //check if the AcademicSemester exits
+  const isAcademicSemesterExits = await AcademicSemester.findById(payload.academicSemester)
+if(!isAcademicSemesterExits){
+throw new AppError(404,"AcademicSemester not found")
+}
+
+
+
+
+
+
+//check duplicate semesterRegistration 
+
+const checkDuplicateSemisterRegistration = await SemesterRegistration.findOne({
+  academicSemester:payload.academicSemester
+})
+if(checkDuplicateSemisterRegistration){
+  throw new AppError(409,"This Semester Registration is alrady register")
+}
+
+
+
+};
 
 const getAllSemesterRegistrationsFromDB = async () => {};
 
