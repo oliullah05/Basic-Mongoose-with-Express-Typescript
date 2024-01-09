025870e49d1ab7c8b82a431f04f251e897b4aa0b
oliullah05 <mdolihasan@gmail.com>
2024-01-09 19:09:19 +0600

    JWT , Create An AccessToken

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,9 @@
+
+import config from "../../config";
 import AppError from "../../errors/AppError";
 import { User } from "../user/user.model";
 import { TLogInUser } from "./auth.interface";
-import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const loginUser = async (payload: TLogInUser) => {
 
@@ -13,32 +15,19 @@ if(!user){
   throw new AppError(404,"user not found")
 }
 
-
-
-
-
-
-
-
-/*
-
 // check if the user is alrady deleted
 
-if(isUserExits.isDeleted){
+if(user.isDeleted){
   throw new AppError(403,"user is deleted")
 }
 
 // check if the user is alrady blocked
 
-if(isUserExits.status==="blocked"){
+if(user.status==="blocked"){
   throw new AppError(403,"user is blocked")
 }
-*/
-
-
 
 // checking if the password is correct
-
 
 const isPasswordMatched =await  User.isPasswordMatched(payload.password,user.password)
 if(!isPasswordMatched){
@@ -46,7 +35,15 @@ if(!isPasswordMatched){
 }
 
 
+//create token and send to the client
+// require('crypto').randomBytes(64).toString('hex')
 
+const jwtPayload ={
+  id:user.id,
+  role:user.role
+}
+
+const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, { expiresIn: '10d'});
 
 
 
