bbfaf54c2192028693c36c11b95693cbb7ab06f9
oliullah05 <mdolihasan@gmail.com>
2024-01-09 15:40:41 +0600

    Implement Business Logic Validation In Auth Service.

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,44 @@
-const loginUser = async (payload: TLoginUser) => {
-  //
-   
-  
+import AppError from "../../errors/AppError";
+import { User } from "../user/user.model";
+import { TLogInUser } from "./auth.interface";
+import bcrypt from "bcrypt"
+
+const loginUser = async (payload: TLogInUser) => {
+
+
+//if the user exits
+const isUserExits = await User.findOne({id:payload.id})
+
+if(!isUserExits){
+  throw new AppError(404,"user not found")
+}
+
+// check if the user is alrady deleted
+
+if(isUserExits.isDeleted){
+  throw new AppError(403,"user is deleted")
+}
+
+// check if the user is alrady blocked
+
+if(isUserExits.status==="blocked"){
+  throw new AppError(403,"user is blocked")
+}
+
+// checking if the password is correct
+const isPasswordMatched =await  bcrypt.compare(payload.password,isUserExits.password)
+console.log(isPasswordMatched,44);
+
+
+
+
+
+
+
+// Access grented : send access token , refresh token
+
+
+
   };
   export const AuthServices = {
     loginUser
--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 const LogInValidationSchema = z.object({
     body: z.object({
         id: z.string({required_error:"Id is required"}),
-        password:z.string({required_error:"Passworde is required"})
+        password:z.string({required_error:"Password is required"})
     })
 })
 
